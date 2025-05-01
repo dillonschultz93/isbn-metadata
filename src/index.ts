afd7c0d79fd79c9fe8e15116a43b1184d8d0abfd
Dillon Schultz <dillonschultz93@gmail.com>
2025-04-30 17:10:39 -0700

    Create a function that fetches data from the Open Library API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { isValidISBN } from './utils/isValidISBN';
-import { GoogleBooksResponse } from './utils/googleBooksTypes';
+import { GoogleBooksResponse, OpenLibraryResponse } from './Types';
 
 // Base URLs to fetch from
 const GOOGLE_BOOKS = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
+const OPEN_LIBRARY = 'https://openlibrary.org/';
 
 // Function to fetch book data from Google Books API
 export async function fetchGoogleBooksData(
@@ -19,5 +20,38 @@ export async function fetchGoogleBooksData(
     if (data.totalItems === 0) throw new Error('No book found with this ISBN');
 
     return data.items[0].volumeInfo;
+  }
+}
+
+export async function fetchOpenLibraryData(
+  query: number | string,
+): Promise<OpenLibraryResponse> {
+  if (!isValidISBN(query)) {
+    throw new Error('This is not a valid ISBN');
+  } else {
+    const response = await fetch(`${OPEN_LIBRARY}/isbn/${query}.json`);
+    if (!response.ok) {
+      throw new Error('No book found with this ISBN');
+    }
+
+    const rawData = await response.json();
+
+    // Hit the Open Library API to get the author names
+    const authors = rawData.authors.map(async (author: { key: string }) => {
+      const resolvedAuthor = await fetch(`${OPEN_LIBRARY}${author.key}.json`);
+      const authorData = await resolvedAuthor.json();
+      return authorData.name;
+    });
+    const authorNames = await Promise.all(authors);
+
+    return {
+      ...rawData,
+      authors: authorNames,
+      cover_imgs: [
+        `https://covers.openlibrary.org/b/isbn/${query}-S.jpg`,
+        `https://covers.openlibrary.org/b/isbn/${query}-L.jpg`,
+        `https://covers.openlibrary.org/b/isbn/${query}-M.jpg`,
+      ],
+    };
   }
 }
